36f8d0b6c979412cf4a98d8064c2d84ab3b5f994
dkwagner <dkwagner@mtu.edu>
2016-03-25 11:38:57 -0400

    Tonys WhamBlam Prefab of Awesomeness

---
+++ Assets/Assets/_Scripts/DB Scripts/Feedback.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/// <summary>
+/// Not using this class, might go back to it later though
+/// </summary>
+public class Feedback : MonoBehaviour {
+    public Text feedbackString;
+
+    private static Feedback instance;
+    private void Awake() {
+        instance = this;
+    }
+
+    public static void setFeedback(string str) {
+        instance.feedbackString.text = "*" + str + "*";
+    }
+
+    public static string getFeedback() {
+        return instance.feedbackString.text;
+    }
+}
